78875ccc5650b72b9e2dc9a3d30c3df59015b4da
imade_ben <imadebensotmi2015@gmail.com>
2020-08-20 12:04:21 +0100

    end begin

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use Request;
+
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -15,6 +17,9 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
+            if (Request::is('admin/*'))
+                return  route('admin.login');
+            else
             return route('login');
         }
     }
